4dfffe285dca41b12b8a7eb062629bc023b4a7d9
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-18 19:07:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,4 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.map! {|element| valid_calls.include?(valid_calls.each)}
-  planeteer_calls[0]
 end
